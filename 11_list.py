f721d865e72d56a49ceded2908df8284c2592a0a
이루가 <Luga@email.com>
2025-09-11 12:49:28 +0900

    list 정렬 및 삭제

--- 11_list.py
+++ 11_list.py
@@ -27,3 +27,23 @@ print(f'a 의 마지막 방의 값={a[3]}')
 print(f'a 의 마지막 방의 값={a[len(a)-1]}')
 # 파이썬에서 사용되는 방식, 0 보다 뒤로가면 맨 뒤로 이동된다는 개념
 print(f'a 의 마지막 방의 값={a[-1]}')
+
+# 리스트 정렬(sort)
+shop_list.sort() # 오름차순
+print(f'shop_list : {shop_list}')
+
+shop_list.sort(reverse=True) # 내림차순
+print(f'shop_list : {shop_list}')
+
+# sorted 는 원본의 리스트를 정렬한 값을 새로운 리스트로 반환
+new_list =sorted(shop_list)
+print(f'new_list : {new_list}')
+
+# a 의 2 번 인덱스에 c 를 넣는다.
+# insert 와 다른점은 해당 인덱스의 값을 지우고 그 자리에 들어간다는 것이다.
+a[2] = 'c'
+print(f'a : {a}')
+
+# list 삭제
+del a[1]
+print(f'a : {a}')
