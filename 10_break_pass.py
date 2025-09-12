0524cf787429c289e1ff6f3622bde8b1060cd0d6
이루가 <Luga@email.com>
2025-09-12 10:43:35 +0900

    tuple 내용

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,11 +1,25 @@
 cup = 0
 running = True
 
-while True:
-    cup += 1
-    print(cup)
-    if cup == 10:
+#while True:
+ #   cup += 1
+  #  print(cup)
+   # if cup == 10:
         #running = False
-        break
+     #   break
 
-print('while 문 종료')
+#print('while 문 종료')
+
+for i in range(1,10):
+    if i ==3 | ==6 |== 9:
+        continue
+    if i ==6:
+        continue
+    if i ==9:
+        continue
+    print(i)
+
+for i in range(1,10):
+    if i % 3 ==0:
+        continue
+    print(i)
