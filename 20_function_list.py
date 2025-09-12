b83ba737093d91c3b870c06e64c621133bd78416
이루가 <Luga@email.com>
2025-09-12 15:39:19 +0900

    함수리스트1

--- 20_function_list.py
+++ 20_function_list.py
@@ -3,6 +3,15 @@
 # 반환타입: X 매개변수: O 모금함
 # 반환타입: X 매개변수: X 리모콘
 
+def 토스트기(bread): # 선언 : 만들어만놨지 누가 사용한건 아님
+    print(f'{bread} 가 구워진다.') # 실질적 동작이 아님, 사람눈에만 보이는거
+    return f'구워진 {bread}' # 실제로 밖으로 나오는 값
+
+#사용
+#dish = 토스트기('종이') # return 으로 나온 값을 dish 에 담는다.
+#print(dish) # dish 안의 값을 출력
+print(토스티기('감자')) # 토스트기에서 나온 값을 바로 출력
+
 #자판기
 def vending(card):
     print(f'{card}로 결제했다')
