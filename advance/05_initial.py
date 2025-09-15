4b9d70bfc3b314bab87fb07b645c2ef253a1fad4
이루가 <Luga@email.com>
2025-09-15 14:28:34 +0900

    클래스 초기화

---
+++ advance/05_initial.py
@@ -0,0 +1,10 @@
+class Puppy:
+
+    name=''"" # 멤버 변수(필드) : class 안에서 사용 가능한 변수
+    goal=""
+
+    def __init__(self,name,goal): # 생성자 : 객체화시 호출되는 함수
+        self.name=name
+        self.goal=goal
+
+puppy = Puppy("멍멍이","집지키기")
