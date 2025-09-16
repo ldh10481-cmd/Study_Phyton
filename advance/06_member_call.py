fe0fa34afcf16556f221cfda0de8b2a8031c8dd2
이루가 <Luga@email.com>
2025-09-16 10:49:23 +0900

    개체사용2

--- advance/06_member_call.py
+++ advance/06_member_call.py
@@ -19,5 +19,14 @@ class Car:
             print('시동이 이미 걸려있습니다.')
 
     def change(self):
-        print(f'{self.gear} 단으로 변속 했습니다.')
+        print(f'{gear} 단으로 변속 했습니다.')
         self.gear += gear
+
+# Car 클래스를 객체화(복사)
+# 객체를 통해 사용하고 싶은 멤버 호출
+car = Car()
+# 시동걸기
+car.start()
+# 변속하기
+car.change(3)
+print(f'현재 car의 gear 단수 : {car.gear}')
