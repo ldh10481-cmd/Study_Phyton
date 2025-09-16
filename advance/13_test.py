f07d62d24caa513bdbbeb4271ab9b4c84ab34166
이루가 <Luga@email.com>
2025-09-16 16:45:59 +0900

    예외 처리 문제

--- advance/13_test.py
+++ advance/13_test.py
@@ -1,3 +1,24 @@
-text = input('값을 넣으면 숫자로 변환 됩니다.')
-num = int(text)
-print(f'당신이 입력한 값 : {num} 이 숫자로 변환 되었습니다.')
+while True:
+    try:
+        text = input('값을 넣으면 숫자로 변환 됩니다.')
+        num = int(text)
+        print(f'당신이 입력한 값:{num} 이 숫자로 변환 되었습니다.')
+        break
+    except ValueError:
+        print(f'{text} 는 숫자가 아닙니다.')
+
+
+
+
+
+
+
+
+# try:
+#     text = input('값을 넣으면 숫자로 변환 됩니다.')
+#     num = int(text)
+#     print(f'당신이 입력한 값 : {num} 이 숫자로 변환 되었습니다.')
+# except ValueError:
+#     print('숫자를 입력해주세요.')
+# finally:
+#     print('끝')
