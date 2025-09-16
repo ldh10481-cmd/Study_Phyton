335550629f4b6757735c71b1ec0e8c6e28ec3514
이루가 <Luga@email.com>
2025-09-16 12:10:42 +0900

    클래스 사용

--- advance/08_inherit.py
+++ advance/08_inherit.py
@@ -15,3 +15,8 @@ class Jumper:
 class Person(Jumper, Runner): # Jumper와 Runer 를 상속 받았다.
     def walk(self):
         print(f'걷는다.')
+
+# walk() 함수를 사용하기 위해 Person 클래스를 객체화 한다.
+p = Person()
+p.walk()
+
